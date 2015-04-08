3d43a9b68307d0275d6abe901cab5fa786064a86
wnatti <wnatti@gmail.com>
2015-04-08 17:00:31 +0300

    Revert "Revert "Revert "Revert "Älä iiro oo tommonen vitun pälli""""
    This reverts commit 05ccf30baf9f7f725a1734df576588da86e51221.

--- Code/A.I. Harjoitustyö/Calculator.h
+++ Code/A.I. Harjoitustyö/Calculator.h
@@ -2,6 +2,16 @@
 #include "BodyPart.h"
 #include "Armor.h"
 #include "Weapon.h"
+#include <math.h>
+
+struct ArmorEfficiency
+{
+	float againstSword;
+	float againstSpear;
+	float againstClub;
+};
+
+
 static class Calculator
 {
 public:
@@ -9,9 +19,12 @@ public:
 	~Calculator();
 
 	float probability(BodyPart hitPart, float dexterity);
-	float damage(Armor armor, Weapon weapon);
+	float damage(Armor armor, Weapon weapon, float strength);
 
 private:
-	float calcTypeEfficiency(ARMOR_TYPE, WEAPON_TYPE);
+	float typeEfficiency(ARMOR_TYPE armorType, WEAPON_TYPE weaponType);
+	float damageValue(float armorValue, float weaponDamage, float strength);
+	float armorEfficiency(ARMOR_TYPE armorType);
 };
+
 
