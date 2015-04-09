6ca95cb78f5ec2ac958932897b70d5d64c125e9c
nolongerme <iiro.heikkinen@hotmail.com>
2015-04-09 16:53:53 +0300

    Calculator nimeämiset kuntoon

--- Code/A.I. Harjoitustyö/Calculator.cpp
+++ Code/A.I. Harjoitustyö/Calculator.cpp
@@ -7,13 +7,12 @@ Calculator::Calculator()
 
 float Calculator::probability(BodyPart hitPart, float dexterity)
 {
-	return BodyPart.chanceToHit*dexterity/10;
+	return hitPart.chanceToHit*dexterity / 10;
 }
 
 float Calculator::damage(Armor armor, Weapon weapon, float strength)
 {
-	typeEfficiency(armor.type, weapon.type) * damageValue(armor.value, weapon.damage, strength);
-	
+	return typeEfficiency(armor.armorType, weapon.weaponType) * damageValue(armor.value, weapon.damage, strength);
 }
 
 float Calculator::damageValue(float armorValue, float weaponDamage, float strength)
@@ -21,7 +20,7 @@ float Calculator::damageValue(float armorValue, float weaponDamage, float streng
 	return weaponDamage * strength * 1/armorValue * 0.1f;
 }
 
-float Calculator::typeEfficiency(ARMOR_TYPE armorType, SWORD_TYPE swordType)
+float Calculator::typeEfficiency(ARMOR_TYPE armorType, WEAPON_TYPE swordType)
 {
 	ArmorEfficiency eff;
 
@@ -32,7 +31,7 @@ float Calculator::typeEfficiency(ARMOR_TYPE armorType, SWORD_TYPE swordType)
 		eff.againstSpear = 1;
 		eff.againstSword = 1.2;
 		break;
-	case CHAIN:
+	case CHAINMAIL:
 		eff.againstClub = 0.8;
 		eff.againstSpear = 1.2;
 		eff.againstSword = 1;
