d0b74e9c6c08c335606ac8d835053d1d1799277b
Allulz <aawave@hotmail.com>
2015-04-14 17:30:28 +0300

    decision ei kokeiltu... tuskin toimii

--- Code/A.I. Harjoitustyö/Decision.cpp
+++ Code/A.I. Harjoitustyö/Decision.cpp
@@ -11,23 +11,23 @@ Decision::~Decision()
 }
 
 
-float Decision::envittutiia(BodyPart bodypart)
+float Decision::envittutiia(BodyPart bodypart, Character character, Character attacker)
 {
-	Calculator::probability(bodypart, float dexterity)
-	Calculator::damage(*part.armor, *attacker.weapon, attacker.strength);
+	float chance = Calculator::probability(bodypart, character.dexterity);
+	float  combined = Calculator::damage(*bodypart.armor, *attacker.weapon, attacker.strength);
 
 	float olenpenis = chance * combined;
 	return olenpenis;
 }
 
-BodyPart Decision::charDecision(BodyPart head, BodyPart rightarm, BodyPart leftarm, BodyPart leftleg, BodyPart rightleg, BodyPart vittutorso)
+BodyPart Decision::charDecision(BodyPart head, BodyPart rightarm, BodyPart leftarm, BodyPart leftleg, BodyPart rightleg, BodyPart vittutorso, Character character, Character attacker)
 {
-	float _head = envittutiia(head);
-	float _righta = envittutiia(leftarm);
-	float _lefta = envittutiia(rightarm);
-	float _rightl = envittutiia(leftleg);
-	float _leftl = envittutiia(rightleg);
-	float _torso = envittutiia(vittutorso);
+	float _head = envittutiia(head, character, attacker);
+	float _righta = envittutiia(leftarm, character, attacker);
+	float _lefta = envittutiia(rightarm, character, attacker);
+	float _rightl = envittutiia(leftleg, character, attacker);
+	float _leftl = envittutiia(rightleg, character, attacker);
+	float _torso = envittutiia(vittutorso, character, attacker);
 
 	vector<float> pilluperkele;
 
@@ -42,24 +42,24 @@ BodyPart Decision::charDecision(BodyPart head, BodyPart rightarm, BodyPart lefta
 	{
 		for (int j = i + 1; j < pilluperkele.size; j++)
 		{
-			if (pilluperkele->at(i) < pilluperkele->at(j))
+			if (pilluperkele[i] < pilluperkele[i])
 			{
-				float temp = pilluperkele->at(j);
-				pilluperkele->at(j) = pilluperkele->at(i);
-				pilluperkele->at(i) = temp;
+				float temp = pilluperkele[j];
+				pilluperkele[j] = pilluperkele[i];
+				pilluperkele[i] = temp;
 			}
 		}
 	}
-	if (pilluperkele->at(0) == _head)
+	if (pilluperkele[0] == _head)
 		return head;
-	if (pilluperkele->at(0) == _lefta)
+	if (pilluperkele[0] == _lefta)
 		return leftarm;
-	if (pilluperkele->at(0) == _righta)
+	if (pilluperkele[0] == _righta)
 		return rightarm;
-	if (pilluperkele->at(0) == _leftl)
+	if (pilluperkele[0] == _leftl)
 		return leftleg;
-	if (pilluperkele->at(0) == _rightl)
+	if (pilluperkele[0] == _rightl)
 		return rightleg;
-	if (pilluperkele->at(0) == _torso)
+	if (pilluperkele[0] == _torso)
 		return vittutorso;
 }
--- Code/A.I. Harjoitustyö/Decision.h
+++ Code/A.I. Harjoitustyö/Decision.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "BodyPart.h"
 #include "Calculator.h"
+#include "Character.h"
 
 
 class Decision
@@ -10,7 +11,7 @@ public:
 	Decision();
 	~Decision();
 
-	float envittutiia(BodyPart bodypart);
-	BodyPart charDecision(BodyPart head, BodyPart rightarm, BodyPart leftarm, BodyPart leftleg, BodyPart rightleg, BodyPart vittutorso);
+	float envittutiia(BodyPart bodypart, Character chracter, Character attacker);
+	BodyPart charDecision(BodyPart head, BodyPart rightarm, BodyPart leftarm, BodyPart leftleg, BodyPart rightleg, BodyPart vittutorso, Character character, Character attacker);
 };
 
